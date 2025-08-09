da10b4c6772b56b43bde29f9a50627d0f9c3a6f3
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-08-09 20:59:22 +0300

    fix test_product_init

--- tests/test_product.py
+++ tests/test_product.py
@@ -8,6 +8,6 @@ def test_product_init(prod_1: Product) -> None:
     assert prod_1.quantity == 5
     assert str(prod_1) == (
                     f''
-                    f"Product(('Samsung Galaxy S23 Ultra', '256GB, Серый цвет, 200MP камера', "
-                    f"{float(180000.0)}, {int(5)}))"
+                    f"Product('Samsung Galaxy S23 Ultra', '256GB, Серый цвет, 200MP камера', "
+                    f"{float(180000.0)}, {int(5)})"
     )
