d8f85dda81d675588019c244620b43a574693b37
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-08-09 22:00:56 +0300

    task 3* done

--- src/product.py
+++ src/product.py
@@ -1,4 +1,4 @@
-from typing import Dict, Type, TypeVar
+from typing import Dict, List, Type, TypeVar
 
 T = TypeVar("T", bound="Product")
 
@@ -24,19 +24,29 @@ class Product:
         self.quantity = quantity
 
     @classmethod
-    def new_product(cls: Type[T], product_params: Dict) -> T:
-        product_obj = cls(
-            product_params["name"],
-            product_params["description"],
-            product_params["price"],
-            product_params["quantity"]
-        )
-        return product_obj
+    def new_product(
+            cls: Type[T],
+            product_params: Dict,
+            products_list: List[T]
+    ) -> T:
+        name = product_params["name"]
+        description = product_params["description"]
+        price = product_params["price"]
+        quantity = product_params["quantity"]
+
+        for product in products_list:
+            # Если товар с тем же именем есть в списке, то обновляем цену и количество
+            if product.name == name:
+                product.quantity += quantity
+                if product.price < price:
+                    product.price = price
+                return product
+
+        new_prod = cls(name, description, price, quantity)
+        products_list.append(new_prod)
+        return new_prod
 
     def __repr__(self) -> str:
-        return f"Product({
-            self.name,
-            self.description,
-            self.price,
-            self.quantity
-        })"
+        return (f"Product("
+                f"'{self.name}', '{self.description}', {self.price}, {self.quantity}"
+                f")")
