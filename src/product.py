4b9e127e5b2e2a4f1ee4f77c4dd00640d26aa459
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-08-09 19:38:47 +0300

    task 3 done

--- src/product.py
+++ src/product.py
@@ -1,3 +1,8 @@
+from typing import Dict, Type, TypeVar
+
+T = TypeVar("T", bound="Product")
+
+
 class Product:
     """Класс для создания продукта."""
 
@@ -17,6 +22,16 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls: Type[T], product_params: Dict) -> T:
+        product_obj = cls(
+            product_params["name"],
+            product_params["description"],
+            product_params["price"],
+            product_params["quantity"]
+        )
+        return product_obj
 
     def __repr__(self) -> str:
         return f"Product({
