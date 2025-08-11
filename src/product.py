b4bf5ce7ded4b40d3f2a4fd3b7d8078a4fe10245
Aksenov Aleksandr <sandroaksyonov@gmail.com>
2025-08-11 14:03:19 +0300

    task 4 done

--- src/product.py
+++ src/product.py
@@ -6,7 +6,7 @@ class Product:
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(
@@ -18,9 +18,21 @@ class Product:
     ) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
         self.__class__.__products_list.append(self)
+
+    @property
+    def price(self) -> float:
+        if self.__price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return self.__price
+        else:
+            return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        self.__price = new_price
 
     @classmethod
     def new_product(cls: Type["Product"], product_params: Dict) -> "Product":
@@ -32,8 +44,8 @@ class Product:
         for product in cls.__products_list:
             if product.name == name:
                 product.quantity += quantity
-                if product.price < price:
-                    product.price = price
+                if product.__price < price:
+                    product.__price = price
                 return product
 
         return cls(name, description, price, quantity)
@@ -41,6 +53,6 @@ class Product:
     def __repr__(self) -> str:
         return (
             f"Product("
-            f"'{self.name}', '{self.description}', {self.price}, {self.quantity}"
+            f"'{self.name}', '{self.description}', {self.__price}, {self.quantity}"
             f")"
         )
